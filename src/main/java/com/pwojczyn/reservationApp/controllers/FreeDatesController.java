7d1410a9184492e1aedebcf3579c570504ca7152
pwojczyn <pwojczyn@gmail.com>
2017-10-10 19:40:02 +0200

    next page

---
+++ src/main/java/com/pwojczyn/reservationApp/controllers/FreeDatesController.java
@@ -0,0 +1,27 @@
+package com.pwojczyn.reservationApp.controllers;
+
+import com.pwojczyn.reservationApp.models.forms.ReservationForm;
+import com.pwojczyn.reservationApp.models.repositories.ReservationRepository;
+import com.pwojczyn.reservationApp.models.services.StringService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+
+@Controller
+public class FreeDatesController {
+    @Autowired
+    StringService stringService;
+
+    @Autowired
+    ReservationRepository reservationRepository;
+
+    @GetMapping("/free-dates")
+    public String freeDatesGet(Model model){
+        model.addAttribute("reservations",reservationRepository.findByDateBetween(LocalDate.now(),LocalDate.now().plusWeeks(4)));
+
+        return "freeDates";
+    }
+}
